ac3503b2871e42ff6c7cc31070f9b5d854e1f6f1
Michael Selsky <Michael@Selsky.me>
2015-11-30 20:12:54 -0500

    Import UIKit to use UIColors
    I'll have to figure out a way around this if I want to make this OSX
    compatible. Perhaps adding a custom struct to represent the RGB
    values of a color and then translate that in the Log struct. Hmmmm
    things to think about.

--- Pod/Classes/Logger.swift
+++ Pod/Classes/Logger.swift
@@ -6,7 +6,7 @@
 //
 //
 
-import Foundation
+import UIKit
 
 public struct Log<T> { //TODO: Rename
     let level: HermesLevel
--- Pod/Classes/XcodeLogger.swift
+++ Pod/Classes/XcodeLogger.swift
@@ -6,7 +6,7 @@
 //
 //
 
-import Foundation
+import UIKit
 
 internal struct XcodeLogger: Logger {
     private let escape = "\u{001b}["
